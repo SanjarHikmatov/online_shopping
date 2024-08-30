a26b6b84371a1df86aea55cfb6d29e3429c1a8ad
sanjar <hikmatovsanjar885@gmail.com>
2024-08-30 09:11:37 +0500

    some things chenget gitignor, config/settings.py, config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from apps.main.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home_page'),
 ]
 
-
+# if settings.DEBUG:
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
